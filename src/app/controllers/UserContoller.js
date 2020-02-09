d1d182bb62bd710e34cb3d9007f259c3ad9635e2
Sidinei Silva <sidinei.dev@icloud.com>
2020-02-09 13:28:56 -0300

    Limit variables returned in create user

--- src/app/controllers/UserContoller.js
+++ src/app/controllers/UserContoller.js
@@ -7,8 +7,13 @@ class UserController {
     if (userExists) {
       return res.status(400).json({ error: 'User already exists.' });
     }
-    const user = await User.create(req.body);
-    return res.json(user);
+    const { id, name, email, provider } = await User.create(req.body);
+    return res.json({
+      id,
+      name,
+      email,
+      provider,
+    });
   }
 }
 
