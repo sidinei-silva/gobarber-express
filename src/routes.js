d858806fe0b2c2751d2f28ae68a8c32c3da95e49
Sidinei Silva <sidinei.dev@icloud.com>
2020-02-09 16:45:21 -0300

    Include create session with JWT

--- src/routes.js
+++ src/routes.js
@@ -2,9 +2,11 @@ import { Router } from 'express';
 
 // Controllers
 import UserContoller from './app/controllers/UserContoller';
+import SessionController from './app/controllers/SessionController';
 
 const routes = new Router();
 
 routes.post('/users', UserContoller.store);
+routes.post('/sessions', SessionController.store);
 
 export default routes;
