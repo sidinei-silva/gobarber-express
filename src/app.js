f9616e01de5c0606a17b4da28f000ae3a396722d
Sidinei Silva <sidinei.silva02@gmail.com>
2020-02-20 12:16:01 -0300

    Send exception to sentry and return errors

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 import 'dotenv/config';
 import express from 'express';
 import 'express-async-errors';
+import Youch from 'youch';
 import path from 'path';
 import * as Sentry from '@sentry/node';
+
 import routes from './routes';
 
 import './database';
@@ -15,6 +17,7 @@ class App {
     Sentry.init(sentryConfig);
     this.middlewares();
     this.routes();
+    this.exceptionHandler();
   }
 
   middlewares() {
@@ -29,6 +32,14 @@ class App {
   routes() {
     this.server.use(routes);
     this.server.use(Sentry.Handlers.errorHandler());
+  }
+
+  exceptionHandler() {
+    this.server.use(async (err, req, res, next) => {
+      const errors = await new Youch(err, req).toJSON();
+
+      return res.status(500).json(errors);
+    });
   }
 }
 
