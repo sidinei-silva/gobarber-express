fd3320f5d32137baea9b9b04a2715ba85a32fc32
Sidinei Silva <sidinei.silva02@gmail.com>
2020-02-20 11:56:20 -0300

    Config Sentry

--- src/app.js
+++ src/app.js
@@ -1,19 +1,23 @@
 import 'dotenv/config';
 import express from 'express';
+import * as Sentry from '@sentry/node';
 import path from 'path';
 import routes from './routes';
 
 import './database';
+import sentryConfig from './config/sentry';
 
 class App {
   constructor() {
     this.server = express();
 
+    Sentry.init(sentryConfig);
     this.middlewares();
     this.routes();
   }
 
   middlewares() {
+    this.server.use(Sentry.Handlers.requestHandler());
     this.server.use(express.json());
     this.server.use(
       '/files',
@@ -23,6 +27,7 @@ class App {
 
   routes() {
     this.server.use(routes);
+    this.server.use(Sentry.Handlers.errorHandler());
   }
 }
 
